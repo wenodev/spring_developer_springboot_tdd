d5957b659ad146c3b0cd406e4f16af15c2c71107
= <=>
2021-11-26 08:40:04 +0900

    test: test exception

--- src/test/java/com/example/SpringbootTdd/CarServiceTest.java
+++ src/test/java/com/example/SpringbootTdd/CarServiceTest.java
@@ -4,6 +4,7 @@ import com.example.SpringbootTdd.domain.Car;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -23,6 +24,19 @@ class CarServiceTest {
 
         //assert
         assertThat(car.getType()).isEqualTo("hybrid");
+    }
+
+    @Test
+    void details메소드는_이름이존재하지않는다면_예외를발생시킨다(){
+        //arrange
+        given(carRepository.findByName("prius")).willReturn(new Car("prius", "hybrid"));
+
+        assertThatThrownBy(() ->
+                //act
+                carService.details("prius1"))
+
+                //asert
+                .isInstanceOf(CarNotFoundException.class);
     }
 
 }
